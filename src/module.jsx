2e790a047957a4dfe21e91a2c5d385842c726c63
nelllyai <worldpiece000@gmail.com>
2023-02-12 22:13:50 +0300

    init

---
+++ src/module.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import girl from './img/test.png';
+
+
+const Module = () => (
+  <>
+  <h1>Hello, React</h1>
+  <img width={500}  src={girl} />
+  </>
+);
+
+export default Module;
